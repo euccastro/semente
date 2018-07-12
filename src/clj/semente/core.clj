e6104891f2bb7de23351353a79cd53f74e80de3a
Estevo U. C. Castro <euccastro@gmail.com>
2018-07-12 04:12:22 +0200

    schema, prova de exponher um web endpoint

---
+++ src/clj/semente/core.clj
@@ -0,0 +1,29 @@
+(ns semente.core
+  (:require
+   [clojure.data.json :as json]
+   [clojure.edn :as edn]
+   [clojure.java.io :as io]
+   [clojure.pprint :as pp]
+   [datomic.client.api :as d]
+   [datomic.ion.lambda.api-gateway :as apigw]))
+
+(def get-conn
+  (memoize #(d/connect
+             (d/client {:server-type :ion
+                        :region "eu-central-1"
+                        :system "deitomique"
+                        :query-group "deitomique"
+                        :endpoint "http://entry.deitomique.eu-central-1.datomic.net:8182/"
+                        :proxy-port 8182})
+             {:db-name "semente"})))
+
+
+(defn webroot*
+  "Lambda ion that returns sample database items matching type."
+  [{:keys [headers body]}]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Olá mundo!"})
+
+(def webroot
+  (apigw/ionize webroot*))
--- src/clj/semente/scratch.clj
+++ src/clj/semente/scratch.clj
@@ -15,7 +15,10 @@
 (def conn (d/connect client {:db-name "semente"}))
 
 (def schema
-  [{:db/ident :user/name
+  [
+   ;; utente
+
+   {:db/ident :user/name
     :db/unique :db.unique/identity
     :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one
@@ -38,22 +41,64 @@
 
    {:db/ident :permission/privilege
     :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/one}])
+    :db/cardinality :db.cardinality/one}
+
+   ;; âmbito
+
+   {:db/ident :scope/display-name
+    :db/unique :db.unique/identity
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "Nome do âmbito para mostrar, em minúsculas"}
+
+   {:db/ident :scope/about
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "ID do elemento elasticsearch onde está o texto da secçom de Quem Somos para este âmbito"}
+
+   {:db/ident :scope/logo-uri
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "ID do URI onde está o logo do âmbito"}
+
+   {:db/ident :scope/facebook-uri
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "URI da página de Facebook deste âmbito, se houver"}
+
+   {:db/ident :scope/youtube-url
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "URI do canal de YouTube deste âmbito, se houver"}
+
+   {:db/ident :scope/twitter-uri
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "URI da página de Twitter deste âmbito, se houver"}
+
+   {:db/ident :scope/centers
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/many
+    :db/isComponent true
+    :db/doc "Centros deste âmbito"}])
 
 (def bootstrap-data
-  [{:db/ident :scope/national}
+  [{:db/ident :scope/national
+    :scope/display-name "nacional"}
    {:db/ident :permission.privilege/admin}])
+
 
 (def eu {:user/name "estevo"
          :user/email "euccastro@gmail.com"
          :user/permission [{:permission/scope :scope/national
                             :permission/privilege :permission.privilege/admin}]})
+(comment
+  (d/transact conn {:tx-data })
 
-(d/transact conn {:tx-data [eu]})
-
-(d/q '[:find ?n
-       :where
-       [?p :permission/privilege :permission.privilege/admin]
-       [?u :user/permission ?p]
-       [?u :user/name ?n]]
-     (d/db conn))
+  (d/q '[:find ?n
+         :where
+         [?p :permission/privilege :permission.privilege/admin]
+         [?u :user/permission ?p]
+         [?u :user/name ?n]]
+       (d/db conn)))
+*1
