a4c6958de8e519e9ce07691ad5f9682612bcb0a0
Estevo U. C. Castro <euccastro@gmail.com>
2019-11-17 13:11:45 +0100

    primeira prova de cljs para editor
    Limpo kee-frame e routing em geral, e acrescento umha prova mínima de re-frame.

--- src/clj/semente/routes/nacional.clj
+++ src/clj/semente/routes/nacional.clj
@@ -1,7 +1,8 @@
 (ns semente.routes.nacional
   (:require
    [semente.db.core :as db]
-   [semente.layout :refer (error-page)]
+   [semente.layout :refer (ok error-page)]
+   [semente.layout.article-editor :refer (article-editor)]
    [semente.layout.nacional :refer (article frontpage)]
    [semente.middleware :as middleware]))
 
@@ -17,6 +18,14 @@
    ["/nacional"
     {:get (fn [_]
             (frontpage (db/get-articles db/crux) db))}]
+   ["/nacional/artigo"
+    {:get (fn [{:keys [query-string]}]
+            (if (= query-string "novo")
+              (article-editor)
+              (error-page
+               {:status 404
+                :title "Que me queres?"
+                :message "Se queres acrescentar um novo artigo, prova /nacional/artigo?novo"})))}]
    ["/nacional/artigo/:unix-name"
     {:get (fn [{{:keys [unix-name]} :path-params}]
             (let [article (some-> unix-name
