196309de293f8c884649414574b4a6901c82c5fc
Estevo U. C. Castro <euccastro@gmail.com>
2019-12-11 10:14:25 +0100

    acrescento o input da data

--- src/cljs/semente/view.cljs
+++ src/cljs/semente/view.cljs
@@ -1,9 +1,16 @@
 (ns semente.view
   (:require
    [re-frame.core :as rf]
-   [reagent.core :as r]))
+   [reagent.core :as r]
+   [semente.prosemirror.view :refer (editor-container)]))
 
 (defn root-component []
-  [:div {:on-click
-         (fn [_] (rf/dispatch [:inc]))}
-   "Provando..." @(rf/subscribe [:prova])])
+  [:div
+   [:div
+    [:label {:for "data"} "data"]
+    [:input.data {:type "date"
+                  :name "data"
+                  :default-value (-> (js/Date.)
+                                     .toISOString
+                                     (.substring 0 10))}]]
+   [editor-container]])
