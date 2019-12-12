e4da6fa37eb100bf53b3d68ed1d7c28d2e2cf52d
Estevo U. C. Castro <euccastro@gmail.com>
2019-12-12 10:51:13 +0100

    acrescento campos que faltavam no formulário

--- src/cljs/semente/view.cljs
+++ src/cljs/semente/view.cljs
@@ -5,19 +5,19 @@
    [reagent.core :as r]
    [semente.prosemirror.view :refer (editor-container)]))
 
+(defn- input [{:keys [id caption tag] :as attrs}]
+  [:div
+   [:label {:for id} (or caption id)]
+   [:div [(or tag :input) (-> attrs (dissoc :tag) (assoc :name id))]]])
+
 (defn root-component []
   [:div
    [:form
-    [:div
-     [:label {:for "data"} "data"]
-     [:input.data {:type "date"
-                   :name "data"
-                   :default-value (-> (js/Date.)
-                                      .toISOString
-                                      (.substring 0 10))}]]
-    [:div
-     [:label {:for "autora"} "autor/a"]
-     [:input.autora {:type "text"
-                     :name "autor/a"
-                     :default-value (j/get js/window :autora)}]]
+    (input {:type "text" :id "titulo" :caption "título"})
+    (input {:type "text" :id "autora" :caption "autor/a" :default-value (j/get js/window :autora)})
+    (input {:type "date" :id "data"
+            :default-value (-> (js/Date.) .toISOString (.substring 0 10))})
+    (input {:type "file" :id "imagem-principal" :caption "imagem principal" :accept "image/*"})
+    (input {:tag :textarea :id "resumo" :cols 60 :rows 3})
+    [:label "corpo"]
     [editor-container]]])
